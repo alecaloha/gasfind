af02b92e28312330605a6c44d214548686c350ef
alecaloha <alecaloha@gmail.com>
2026-04-14 20:59:31 -0400

    Log raw HTML response for debugging

--- api/stockr.js
+++ api/stockr.js
@@ -14,6 +14,7 @@ export default async function handler(req, res) {
     }
 
     const html = await response.text();
+    console.log("RAW HTML:", html.slice(0, 500));
 
     const predictions = [];
 
